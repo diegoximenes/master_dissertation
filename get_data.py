ff4199304b520c4870cab2dabf8645e94b86f8d4
Diego Ximenes <dxmendes1@gmail.com>
2016-10-16 16:54:50 -0200

    Get all documents from mongodb to file.

--- get_data.py
+++ get_data.py
@@ -9,7 +9,7 @@ import utils.dt_procedures as dt_procedures
 import utils.db_procedures as db_procedures
 
 
-def create_dirs(date_dir, server):
+def create_dirs_csv(date_dir, server):
     for dir in ["{}/input/".format(base_dir),
                 "{}/input/{}".format(base_dir, date_dir),
                 "{}/input/{}/{}/".format(base_dir, date_dir, server)]:
@@ -17,22 +17,13 @@ def create_dirs(date_dir, server):
             os.makedirs(dir)
 
 
-def get_data(dt_start_sp, dt_end_sp):
-    """
-    dt_start_sp and dt_end_sp must define a month in sao paulo time
-    """
+def create_dirs_doc():
+    for dir in ["{}/input_doc/".format(base_dir)]:
+        if not os.path.exists(dir):
+            os.makedirs(dir)
 
-    target_year = dt_start_sp.year
-    target_month = dt_start_sp.month
 
-    client = MongoClient("cabul", 27017)
-    collection = client["NET"]["measures"]
-
-    date_dir = "{}_{}".format(target_year, str(target_month).zfill(2))
-    dt_start = dt_procedures.from_sp_to_utc(dt_start_sp)
-    dt_end = dt_procedures.from_sp_to_utc(dt_end_sp)
-
-    cursor = collection.find({"_id.date": {"$gte": dt_start, "$lt": dt_end}})
+def write_to_csv(date_dir, dt_start, dt_end, cursor, collection):
     macs, servers = db_procedures.get_macs(cursor)
 
     cnt = 0
@@ -40,7 +31,7 @@ def get_data(dt_start_sp, dt_end_sp):
         cnt += 1
         print "mac={}, cnt={}".format(mac, cnt)
 
-        create_dirs(date_dir, server)
+        create_dirs_csv(date_dir, server)
 
         cursor = collection.find({"$and": [{"_id.date": {"$gte": dt_start,
                                                          "$lt": dt_end}},
@@ -75,12 +66,49 @@ def get_data(dt_start_sp, dt_end_sp):
                                               nominal_up, nominal_down,
                                               traceroute))
 
+
+def write_docs(date_dir, cursor):
+    create_dirs_doc()
+    out_path = "{}/input_doc/{}.txt".format(base_dir, date_dir)
+    with open(out_path, "w") as f:
+        cnt = 0
+        for doc in cursor:
+            cnt += 1
+            print "cnt={}".format(cnt)
+
+            if db_procedures.valid_doc(doc):
+                f.write("{}\n".format(doc))
+
+
+def get_data(dt_start_sp, dt_end_sp, to_csv):
+    """
+    dt_start_sp and dt_end_sp must define a month in sao paulo time
+    """
+
+    target_year = dt_start_sp.year
+    target_month = dt_start_sp.month
+
+    client = MongoClient("cabul", 27017)
+    collection = client["NET"]["measures"]
+
+    date_dir = "{}_{}".format(target_year, str(target_month).zfill(2))
+    dt_start = dt_procedures.from_sp_to_utc(dt_start_sp)
+    dt_end = dt_procedures.from_sp_to_utc(dt_end_sp)
+
+    cursor = collection.find({"_id.date": {"$gte": dt_start, "$lt": dt_end}})
+
+    if to_csv:
+        write_to_csv(date_dir, dt_start, dt_end, cursor, collection)
+    else:
+        write_docs(date_dir, cursor)
+
+
 if __name__ == "__main__":
     # dt_start_sp = datetime(2016, 8, 1, 0, 0, 0)
     # dt_end_sp = datetime(2016, 9, 1, 0, 0, 0)
     # get_data(dt_start_sp, dt_end_sp)
 
-    for month in range(6, 10):
+    for month in range(7, 10):
         dt_start_sp = datetime(2016, month, 1, 0, 0, 0)
         dt_end_sp = datetime(2016, month + 1, 1, 0, 0, 0)
-        get_data(dt_start_sp, dt_end_sp)
+        get_data(dt_start_sp, dt_end_sp, to_csv=False)
