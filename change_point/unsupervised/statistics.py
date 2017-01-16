b4a284227d792dbc1c515396acdcd403e16537aa
Diego Ximenes <dxmendes1@gmail.com>
2017-01-16 02:53:51 -0200

    statistics script update

--- change_point/unsupervised/statistics.py
+++ change_point/unsupervised/statistics.py
@@ -1,106 +1,53 @@
 import os
 import sys
-import datetime
-import functools
-import ast
 import pandas as pd
 
 script_dir = os.path.join(os.path.dirname(__file__), ".")
 base_dir = os.path.join(os.path.dirname(__file__), "../..")
 sys.path.append(base_dir)
-import utils.utils as utils
-import change_point.cp_utils.cp_utils as cp_utils
 
 
-def problem_location(dt_start, dt_end):
-    def problem_before(in_path):
-        df = pd.read_csv(in_path)
-        cnt_all = df.shape[0]
-        df_before = df[df["problem_location"] == "['before']"]
-        cnt_before = df_before.shape[0]
-        df_before = df[df["cnt_clients"] > 1]
-        cnt_before_more_than_one_client = df_before.shape[0]
-        return cnt_all, cnt_before, cnt_before_more_than_one_client
-
-    str_dt = utils.get_str_dt(dt_start, dt_end)
-
-    out_path = "{}/prints/{}/filtered/problem_location_statistics.csv".format(
-        script_dir, str_dt)
-    with open(out_path) as f:
-        f.write("cnt_all,"
+def statistics():
+    out_path = "{}/prints/problem_location_statistics.csv".format(script_dir)
+    with open(out_path, "w") as f:
+        f.write("metric,"
                 "cnt_before,"
-                "cnt_before_more_than_one_client,"
-                "cnt_not_before,"
-                "cnt_not_before_more_than_one_zero_indegree_vertex\n")
+                "cnt_zero_indegree_without_correlation,"
+                "cnt_zero_indegree_with_correlation\n")
 
-        in_path = ("{}/prints/{}/filtered/{}/"
-                   "problem_location_first_hop_not_zero_indegree_vertex.csv".
-                   format(script_dir, str_dt, metric))
-        df = pd.read_csv(in_path)
-        df_before = df[df["problem_location"] == "['before']"]
-        cnt_all = df_before.shape[0]
-        cnt_before = df_before.shape[0]
-        df_before = df[df["cnt_clients"] > 1]
-        cnt_before_more_than_one_client = df_before.shape[0]
+        for metric in ["latency", "loss", "throughput_up"]:
+            in_path = ("{}/prints/"
+                       "problem_location_first_hop_not_zero_indegree_vertex"
+                       ".csv".format(script_dir))
+            df = pd.read_csv(in_path)
+            df = df[df["metric"] == metric]
+            df_before = df[df["problem_location"] == "['before']"]
+            cnt_before = df_before.shape[0]
 
-        in_path = ("{}/prints/{}/filtered/{}/"
-                   "problem_location_zero_indegree_vertexes_correlation.csv".
-                   format(script_dir, str_dt, metric))
+            in_path = ("{}/prints/"
+                       "problem_location_zero_indegree_vertexes_correlation"
+                       ".csv".format(script_dir))
+            df = pd.read_csv(in_path)
+            df = df[df["metric"] == metric]
+            df_before = df[df["suffix_match"] == "['before']"]
+            cnt_before += df_before.shape[0]
+            df_not_before = df[df["suffix_match"] != "['before']"]
+            cnt_zero_in_degree_without_correlation = \
+                df_not_before[
+                    df_not_before[
+                        "cnt_vertexes_with_zero_indegree"] == 1].shape[0]
+            cnt_zero_in_degree_with_correlation = \
+                df_not_before[
+                    df_not_before[
+                        "cnt_vertexes_with_zero_indegree"] > 1].shape[0]
 
-        df = pd.read_csv(in_path)
-        cnt_all += df.shape[0]
-        df = df[df["problem_location"] == "['before']"]
-        for _, row in df.iterrows():
-            dic = ast.literal_eval(row["vertexes_with_zero_indegree"])
-            cnt_before += 1
-            if dic["cnt_clients"] > 1:
-                cnt_before_more_than_one_client += 1
-
-        df = pd.read_csv(in_path)
-        df = df[df["problem_location"] != "['before']"]
-        cnt_not_before = df.shape[0]
-        df = df[df["cnt_vertexes_with_zero_indegree"] > 1]
-        cnt_not_before_more_than_one_zero_indegree_vertex = df.shape[0]
-
-        l = "{}" + ",{}" * 5
-        l = l.format(cnt_all,
-                     cnt_before,
-                     cnt_before_more_than_one_client,
-                     cnt_not_before,
-                     cnt_not_before_more_than_one_zero_indegree_vertex)
-        f.write(l)
-
-
-def statistics(dt_start, dt_end, metric):
-    problem_location(dt_start, dt_end, metric)
-
-
-def run_sequential(metric, eps_hours):
-    for dt_start, dt_end in utils.iter_dt_range():
-        statistics(dt_start, dt_end, metric)
-
-
-def run_parallel(metric, eps_hours):
-    dt_ranges = list(utils.iter_dt_range())
-    f_statistics = functools.partial(statistics, metric=metric)
-    utils.parallel_exec(f_statistics, dt_ranges)
-
-
-def run_single(dt_start, dt_end, metric):
-    statistics(dt_start, dt_end, metric)
+            l = "{}" + ",{}" * 3 + "\n"
+            l = l.format(metric,
+                         cnt_before,
+                         cnt_zero_in_degree_without_correlation,
+                         cnt_zero_in_degree_with_correlation)
+            f.write(l)
 
 
 if __name__ == "__main__":
-    metric = "latency"
-    dt_start = datetime.datetime(2016, 5, 1)
-    dt_end = datetime.datetime(2016, 5, 11)
-
-    parallel_args = {"metric": metric}
-    sequential_args = parallel_args
-    single_args = {"dt_start": dt_start,
-                   "dt_end": dt_end}
-    single_args.update(parallel_args)
-    cp_utils.parse_args(run_single, single_args,
-                        run_parallel, parallel_args,
-                        run_sequential, sequential_args,
-                        None, None)
+    statistics()
